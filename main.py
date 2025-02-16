3eb17adb37f6b07dbf34b7bfe778c965e8ff1d2c
dan-land <danielcoffland@gmail.com>
2025-02-15 23:49:48 -0800

    Added basic render of  player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
-
+from circleshape import *
+from player import *
 
 
 def main():
@@ -8,7 +9,10 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    asteroid_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -16,7 +20,10 @@ def main():
                 return
 
         screen.fill("Black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = (asteroid_clock.tick(60) / 1000)
+
 
 if __name__ == "__main__":
     main()
